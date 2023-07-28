b4d7f48e46744d61585d6f8ec898bc30cf3f34df
sergeev1706 <140341803+sergeev1706@users.noreply.github.com>
2023-07-28 22:33:18 +0300

    add new files

--- index.js
+++ index.js
@@ -3,12 +3,30 @@
 import http from 'http';
 import fs from 'fs'
 
-http.createServer(async (request, response) => {
-  if (request.url != '/favicon.ico') {
-    let text = await fs.promises.readFile('page.html', 'utf8');
+let object = {
+  '/page1': 'file1.html',
+  '/page2': 'file2.html',
+  '/page3': 'file3.html',
+}
 
-    response.writeHead(200, { 'Content-Type': 'text/html' });
+http.createServer(async (request, response) => {
+
+  let text = 'page not found';
+  let status = 404;
+
+  if (request.url !== '/favicon.ico') {
+    for (const key in object) {
+
+      if (request.url === key) {
+        text = await fs.promises.readFile(object[key], 'utf8');
+        status = 200;
+      }
+    }
+    response.writeHead(status, { 'Content-Type': 'text/html' });
     response.write(text);
     response.end();
+
   }
 }).listen(3000);
+
+
